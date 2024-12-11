b9f75e241704f216e7329439dde52b94d5b3751f
eng-AhmedMahmoud <ahmeddahabmohamed@gmail.com>
2024-12-11 20:19:16 +0200

    feat: basic updates

--- settings/settings.ts
+++ settings/settings.ts
@@ -1,10 +1,10 @@
-export const url = "https://rubix-documents.vercel.app";
+export const url = "https://redux-toolkit-migeration.vercel.app/";
 export const siteicon = "/images/icon.png";
 
 export const gtm = "GTM-XXXXXXX";
 export const gtmconnected = true;
 
-export const sitename = "Redux Toolkit Migeration";
+export const sitename = "Redux Toolkit Migration";
 export const description = "This comprehensive documentation template, crafted with Next.js and available as open-source, delivers a sleek and responsive design, tailored to meet all your project documentation requirements.";
 export const keywords = ["Next.js", "documentation", "Redux Toolkit Migeration", "open source", "SEO"];
 export const urlimage = "/images/og-image.png";
